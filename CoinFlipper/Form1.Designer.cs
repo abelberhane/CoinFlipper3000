d644710fc8cded87b8ae58aa81ce069de368d813
Abel Berhane <aberhane@datadimensions.com>
2017-06-26 12:22:19 -0400

    Finished the coin flipper and added some aesthetics

---
+++ CoinFlipper/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace CoinFlipper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblHeads = new System.Windows.Forms.Label();
+            this.lblTails = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button1.Location = new System.Drawing.Point(95, 72);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Flip!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblHeads
+            // 
+            this.lblHeads.AutoSize = true;
+            this.lblHeads.BackColor = System.Drawing.SystemColors.Menu;
+            this.lblHeads.Location = new System.Drawing.Point(104, 213);
+            this.lblHeads.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblHeads.Name = "lblHeads";
+            this.lblHeads.Size = new System.Drawing.Size(46, 15);
+            this.lblHeads.TabIndex = 1;
+            this.lblHeads.Text = "Heads";
+            // 
+            // lblTails
+            // 
+            this.lblTails.AutoSize = true;
+            this.lblTails.BackColor = System.Drawing.SystemColors.Menu;
+            this.lblTails.Location = new System.Drawing.Point(257, 213);
+            this.lblTails.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTails.Name = "lblTails";
+            this.lblTails.Size = new System.Drawing.Size(34, 15);
+            this.lblTails.TabIndex = 2;
+            this.lblTails.Text = "Tails";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Menu;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(92, 184);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Heads";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Menu;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(243, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tails";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Window;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label3.Location = new System.Drawing.Point(92, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(201, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Press the Flip button to begin.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(379, 301);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTails);
+            this.Controls.Add(this.lblHeads);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Lucida Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Coin Flipper 3000";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblHeads;
+        private System.Windows.Forms.Label lblTails;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CoinFlipper/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoinFlipper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //This is where we create the random logic to pick a random number.
+        private static Random rand = new Random();
+
+        //The Flip logic stating the randoms choice is between 0 or 1. Then giving true or false values for them.
+        private bool Flip()
+        {
+            int number = rand.Next(0, 2);
+            if (number == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        //Here we assign the labels values of 0 to start. 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblHeads.Text = "0";
+            lblTails.Text = "0";
+        }
+
+        //The button logic to increment the Flip Counters based on if isHeads is true. If the rand above decides its heads, we increment Heads. If not, Tails.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool isHeads = Flip();
+            if (isHeads)
+            {
+                lblHeads.Text = (int.Parse(lblHeads.Text) + 1).ToString();
+            }
+            else
+            {
+                lblTails.Text = (int.Parse(lblTails.Text) + 1).ToString();
+            }
+        }
+    }
+}
